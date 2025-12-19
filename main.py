8a35ff9bf5c01ccfea3ddcb7e6cc064485e4d4b6
Girish-W223 <girishwaghchoure025@gmail.com>
2025-12-19 13:02:43 +0530

    added put and get

--- main.py
+++ main.py
@@ -8,14 +8,39 @@ Students=[
     Student(id=1,name="pqr",percentage=60.99)
 ]
 
-@app.get('/')
+@app.get('/student')
 def get_all():
     return Students 
 
-@app.post('/')
+@app.post('/student')
 def add_student(stud:Student):
         Students.append(stud)
         return {"addition successful"}
     
-    
-    
+@app.get('/student/{id}')
+def get_one(id:int):
+    for stud in Students:
+        if stud.id==id:
+            return stud  
+    return{"student not found"}
+
+
+
+
+@app.delete('/student/{id}')
+def delete(id:int):
+    for i,stud in enumerate(Students):
+          if stud.id==id:
+               Students.pop(i)
+               return{"message:deleted successfully"}
+    return{"message: student not found "}
+
+
+
+@app.put('/student/{id}')
+def update(id:int,u_stud:Student):
+    for i,stud in enumerate(Students):
+        if stud.id==id:
+            Students[i]=u_stud 
+            return{"message":"student updated"}
+    return{"message: student not found "}
